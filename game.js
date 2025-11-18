42dc8c4072698c19d79a38f503845be040acf9cb
Fish <115324554+Bwaf@users.noreply.github.com>
2025-11-17 16:11:41 -0800

    changing the player to blue

--- game.js
+++ game.js
@@ -29,7 +29,7 @@ function gameLoop() {
     if (keys["ArrowRight"]) x += speed;
 
     // Draw red square
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "blue";
     ctx.fillRect(x, y, 50, 50);
 
     requestAnimationFrame(gameLoop);
